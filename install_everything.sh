4943dcd832c443f0c4a22e675ffe08d0fae0e4ec
Tommy P. Keane <talk.to.tommypkeane@gmail.com>
2025-10-01 08:13:21 -0400

    [NEW] Auto-Installation Script
    - Tested only on macOS
    - Final steps may overwrite critical configurations on a Linux system,
    so you may not want to just run this anywhere

---
+++ install_everything.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env bash
+# Installation Script for macOS (or Linux with `brew`) of all recommended tools
+#
+# References:
+#	- https://github.com/TommyPKeane/example-bash-configuration
+#	- https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
+#	- https://stackoverflow.com/questions/23424783/ostype-not-available-in-shell-script
+#	- https://www.redhat.com/en/blog/exit-codes-demystified
+
+set -eu
+
+# OS-specific Installations
+#
+#	- macOS: XCode Utilities need to be installed
+#	- Linux: N/A
+#	- Windows: N/A
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	; # pass
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	xcode-select --install
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+	; # pass
+elif [[ "$OSTYPE" == "msys" ]]; then
+	; # pass
+elif [[ "$OSTYPE" == "win32" ]]; then
+	; # pass
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+	; # pass
+else
+	if [[ $(uname) == "Darwin" ]]; then
+		xcode-select --install
+	else
+		>&2 echo "⚠️ Could not determine Operating System | Exiting"
+		exit 1
+	fi
+fi
+
+
+# brew Install
+if ! command -v brew >/dev/null 2>&1; then
+	echo "⬇️ Installing brew ..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	echo "✅ brew Already Installed"
+fi
+brew update
+brew upgrade
+
+
+# Code Building and Management
+brew install gcc
+brew install glib
+brew install pkgconfig
+brew install git
+brew install bash-completion
+brew install git-svn
+brew install git-gui
+brew install make  # Installs to alias `gmake`
+brew install cmake
+
+
+# Basic Utilities
+brew install rsync
+brew install ack
+brew install jq
+brew install yq
+brew install jless
+brew install direnv
+brew install fzf
+brew install tree
+brew install grep  # Installs to alias `ggrep`
+
+
+# Unix Libraries
+brew install openssl
+brew install readline
+brew install zlib
+
+
+# Shell Customization
+brew install starship
+
+
+# Image Processing Utilities
+brew install exiftool
+brew install imagemagick
+brew install graphicsmagick
+
+
+# Video Processing Utilities
+brew install mkvtoolnix
+brew install ffmpeg
+brew install yt-dlp
+
+
+# Python Utilities
+brew install pyenv
+brew install pyenv-virtualenv
+brew install uv
+mkdir -p ~/.local/bin/
+
+
+# Rust Utilities
+brew install rust
+brew install rustup
+
+
+# Node.js Utilities
+brew install nvm
+
+
+# Kubernetes Utilities
+brew install kubectl
+brew install minikube
+
+
+# Terraform Utilities
+brew install warrensbox/tap/tfswitch
+
+
+# Docker and Containerization Utilities
+brew install dive
+
+
+# Bash Configs Overwrite
+echo "💾 Overwriting bash Configuration Files..."
+cp -Rv ./.bashrc.d/ ~/.bashrc.d
+cp -v ./.bashrc ~/
+cp -v ./.bash_profile ~/
+
+
+# Update current Shell without needing to close Terminal
+source ~/.bashrc
